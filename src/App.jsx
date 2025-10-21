c732f677201628b3e77b0cd54a0aeaf03971e60f
AchiraStudio <107109072+AchiraStudio@users.noreply.github.com>
2025-10-21 14:35:06 +0700

    Update all

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./elements/pages/main";
-// import ChristmasEvent from './elements/pages/event-1';
-// import Charity from './elements/pages/event-2';
-// import Mpls from './elements/pages/event-3';
-// import Valentine from './elements/pages/event-4';
-// import Pensi from './elements/pages/pensi/main-pensi';
+import ChristmasEvent from './elements/pages/event-1';
+import Charity from './elements/pages/event-2';
+import Mpls from './elements/pages/event-3';
+import Valentine from './elements/pages/event-4';
+import Pensi from './elements/pages/pensi/main-pensi';
 import NotFound from './elements/pages/NotFound';
 
 function App() {
@@ -12,11 +12,11 @@ function App() {
     <BrowserRouter basename="/recis-shs/">
       <Routes>
         <Route path="/" element={<Main />} />
-        {/* <Route path="/Christmas" element={<ChristmasEvent />} />
+        <Route path="/Christmas" element={<ChristmasEvent />} />
         <Route path="/Charity" element={<Charity />} />
         <Route path="/Mpls" element={<Mpls />} />
         <Route path="/Valentine" element={<Valentine />} />
-        <Route path="/Pensi" element={<Pensi />} /> */}
+        <Route path="/Pensi" element={<Pensi />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
